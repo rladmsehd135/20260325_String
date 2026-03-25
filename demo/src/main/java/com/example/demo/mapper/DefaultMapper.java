e1f66d11edb93d9909cf2c8be5b9e00469899ec4
eundong <rladmsehd135@gmail.com>
2026-03-25 18:21:54 +0900

    2026 03 25

---
+++ demo/src/main/java/com/example/demo/mapper/DefaultMapper.java
@@ -0,0 +1,13 @@
+package com.example.demo.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.example.demo.model.User;
+
+@Mapper
+public interface DefaultMapper {
+	public List<User> selectUserList();
+	
+}
